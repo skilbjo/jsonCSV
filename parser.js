69d8cf48f4cfd95e24303b51630ee59d46838735
John Skilbeck <jskilbeck@001224-mac.local>
2015-04-15 12:56:01 -0700

    finished json parser

---
+++ parser.js
@@ -0,0 +1,19 @@
+var fs 					= require('fs')
+	, inFile 			= './people.json'
+	, outFile			= './people.csv'
+	, json2csv 		= require('json2csv')
+	;
+
+fs.readFile(inFile, 'utf-8', function(err, fileContents){
+	var jsonData = JSON.parse(fileContents);
+
+	json2csv({data: jsonData, fields: ['full_name','company_name','job_title']}, function(err, csv) {
+		if (err) throw err;
+		fs.writeFile(outFile, csv, function(err){
+			if (err) throw err;
+			console.log('file saved');
+		})
+	})
+});
+
+
